397adfdfb7873bcc580993c7e5e44d6506fba47e
EdoCosic <edo.cosic@edu.fit.ba>
2025-11-29 15:29:16 +0100

    implement home page, registration form & component communication

--- Client/src/app/app.ts
+++ Client/src/app/app.ts
@@ -3,10 +3,12 @@ import { HttpClient } from '@angular/common/http';
 import { lastValueFrom } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
 import { AccoutService } from '../core/services/accout-service';
+import { Home } from "../features/home/home";
+import { User } from '../types/user';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav],
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -14,7 +16,7 @@ export class App implements OnInit{
   private accoutService = inject(AccoutService);
   private http = inject(HttpClient);
   protected title = 'GlobalFit';
-  protected members = signal<any>([]) 
+  protected members = signal<User[]>([]) 
 
   async ngOnInit() {
     this.members.set(await this.getmembers());
@@ -30,7 +32,7 @@ export class App implements OnInit{
 
   async getmembers() {
     try {
-      return await lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+      return await lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
     } catch (error) {
       console.error(error);
       throw error;
