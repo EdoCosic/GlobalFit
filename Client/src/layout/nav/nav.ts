5f28c604d2e2c9ecac7c387357d7d6eea9bc62e9
EdoCosic <edo.cosic@edu.fit.ba>
2025-12-10 16:09:41 +0100

    added member types and theme switching functionality

--- Client/src/layout/nav/nav.ts
+++ Client/src/layout/nav/nav.ts
@@ -1,20 +1,35 @@
-import { Component, inject, signal, Signal } from '@angular/core';
+import { Component, inject, OnInit, signal, Signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccoutService } from '../../core/services/accout-service';
 import { Router, RouterLink, RouterLinkActive } from "@angular/router";
 import { ToastService } from '../../core/services/toast-service';
-
+import { themes } from '../theme';
 @Component({
   selector: 'app-nav',
   imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
-export class Nav {
+export class Nav implements OnInit {
   protected accountService = inject(AccoutService);
   private router = inject(Router);
   private toast = inject(ToastService);
   protected creds: any = {}
+  protected selectedTheme = signal<string>(localStorage.getItem('theme') || 'light');
+  protected themes = themes;
+
+  ngOnInit(): void {
+    document.documentElement.setAttribute('data-theme', this.selectedTheme());
+  }
+
+  handleSelectTheme(theme: string) {
+    this.selectedTheme.set(theme);
+    localStorage.setItem('theme', theme);
+    document.documentElement.setAttribute('data-theme', theme);
+    const elem = document.activeElement as HTMLDivElement;
+    if (elem) elem.blur();
+  }
+
 
   login() {
     this.accountService.login(this.creds).subscribe({
